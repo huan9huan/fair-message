e3a4d51a542b75a7e7bc0c7ec688d9ac4c03bcd7
alhuang <alhuang@marinsoftware.com>
2019-01-14 21:54:06 +0800

    save config into local storage

--- src/scripts/local.js
+++ src/scripts/local.js
@@ -1,22 +1,25 @@
 import storage from "./utils/storage";
 
-var DEFAULT_CONFIG = {
+const DEFAULT_CONFIG = {
     weibo: {auto: false},
     twitter:  {auto: false},
-  };
+};
   
-var Config = Object.assign({}, ...DEFAULT_CONFIG);
+var Config = Object.assign({}, DEFAULT_CONFIG);
 
 function loadConfig() {
     storage.get('config', function(resp) {
         if(resp.config) {
             Config = resp.config;
             console.log("load config from storage", Config);
+        }else{
+            console.log("config use default", Config);
         }
     });  
 }
 
-export function saveConfig() {
+export function saveConfig(config) {
+    Config = config;
     storage.set({config}, function(resp) {
         console.log("save config done", config);
     });  
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -8,7 +8,7 @@ var template = (data) => {
     <h3 class="title">${data.count}</h3>
     <div>
       <a href="${data.url}" class="url">${data.url}</a>
-      <input type="checkbox" checked>Auto Hide</input>
+      <input type="checkbox" id="auto_hide_${data.site}">Auto Hide</input>
     </div>
    </div>
   `);
@@ -19,11 +19,27 @@ var renderMessage = (message) => {
   displayContainer.innerHTML = `<p class='message'>${message}</p>`;
 }
 
-var renderCnt = (data) => {
+function withConfig(site) {
+  var autoHideElement = document.querySelector(`#auto_hide_${site}`);
+  if(autoHideElement){
+    var config = getConfig();
+    autoHideElement.checked = (config[site] || {}).auto || false;
+    autoHideElement.addEventListener("click", function(e) {
+      config[site].auto = !(config[site].auto);
+      autoHideElement.checked = config[site].auto
+      saveConfig(config);
+    });
+  }
+}
+function reanderFor(site, data) {
+  renderContent(Object.assign({site}, data));
+}
+var renderContent = (data) => {
   var displayContainer = document.getElementById("display-container")
   if(data) {
     var tmpl = template(data);
     displayContainer.innerHTML = tmpl;  
+    withConfig(data.site);
   } else {
     renderMessage("Sorry, could not connect with page")
   }
@@ -33,26 +49,26 @@ ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
   var activeTab = tabs[0];
   var url = activeTab.url;
   if(url && url.indexOf("twitter.com")){
-    chrome.tabs.sendMessage(activeTab.id, { action: 'hide-twitter' }, renderCnt);
+    chrome.tabs.sendMessage(activeTab.id, { action: 'hide-twitter' }, reanderFor.bind(this, "twitter"));
   }
   if(url && url.indexOf("weibo.com")){
-    chrome.tabs.sendMessage(activeTab.id, { action: 'hide-weibo' }, renderCnt);
+    chrome.tabs.sendMessage(activeTab.id, { action: 'hide-weibo' }, reanderFor.bind(this, "weibo"));
   }
 });
 
-popup.addEventListener("click", function(e) {
-  if(e.target && e.target.matches("#save-btn")) {
-    e.preventDefault();
-    var data = e.target.getAttribute("data-bookmark");
-    ext.runtime.sendMessage({ action: "perform-save", data: data }, function(response) {
-      if(response && response.action === "saved") {
-        renderMessage("Your bookmark was saved successfully!");
-      } else {
-        renderMessage("Sorry, there was an error while saving your bookmark.");
-      }
-    })
-  }
-});
+// popup.addEventListener("click", function(e) {
+//   if(e.target && e.target.matches("#save-btn")) {
+//     e.preventDefault();
+//     var data = e.target.getAttribute("data-bookmark");
+//     ext.runtime.sendMessage({ action: "perform-save", data: data }, function(response) {
+//       if(response && response.action === "saved") {
+//         renderMessage("Your bookmark was saved successfully!");
+//       } else {
+//         renderMessage("Sorry, there was an error while saving your bookmark.");
+//       }
+//     })
+//   }
+// });
 
 var optionsLink = document.querySelector(".js-options");
 optionsLink.addEventListener("click", function(e) {
